2f937ee744e3b2f58af918b62606f8928bd5309b
Dariusz Aniszewski <dariusz.aniszewski@polidea.com>
2015-05-11 19:05:25 +0200

    added readme, updated setup.py and gitignore

---
+++ django_pay/README.md
@@ -0,0 +1,12 @@
+# Django Pay
+
+Simple Django module for easy integration of online payments in your application.
+
+### Supporting Providers
+
+- PayU
+
+
+### Planned
+
+- others ;)
--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_pay',
-    version='0.0.1',
+    version='0.0.2',
     author=u'Dariusz Aniszewski',
     author_email='dariusz@aniszewski.eu',
     packages=find_packages(),
     url='https://github.com/DariuszAniszewski/django-pay',
     license='TBD',
     description='TBD',
+    install_requires=['pytz'],
     zip_safe=False,
 )
