35775f6e7e23476d2a03013650bbfb115e48702f
Dariusz Aniszewski <dariusz.aniszewski@polidea.com>
2015-06-01 15:10:57 +0200

    dependencies done right

--- setup.py
+++ setup.py
@@ -7,13 +7,17 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-payu',
-    version='0.2.4',
+    version='0.2.5',
     author=u'Dariusz Aniszewski',
     author_email='dariusz@aniszewski.eu',
     packages=find_packages(),
     url='https://github.com/DariuszAniszewski/django-payu',
     license='TBD',
     description='Minimalistic PayU wrapper.',
-    install_requires=['pytz==2015.2', 'six==1.4.1', 'requests==2.7.0'],
+    install_requires=[
+        'pytz==2015.4',
+        'six==1.9.0',
+        'requests==2.7.0'
+    ],
     zip_safe=False,
 )
